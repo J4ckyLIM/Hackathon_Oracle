982cdda0283139d36245e59183baa48f5e6dcb39
Teddy Jean-Francois <teddy.jean-francois@ibm.com>
2022-04-08 12:22:53 +0200

    Push IPFS & Set Data

--- src/index.ts
+++ src/index.ts
@@ -4,20 +4,19 @@ import { getTickerPrice } from './useCases/price/getTickerPrice';
 import { LocalDate } from '@js-joda/core';
 import { logger } from './infrastructure/logger';
 import { ethers } from 'ethers';
-import { TestContract } from './abi';
+import { Contract } from './abi';
 import { InfuraProvider } from '@ethersproject/providers';
 
 import * as IPFS from "ipfs-http-client";
 
 const POLYGON_API_KEY = 'x3B2H9yFehXvytQcJwjY0gpBviqZhrvO';
-const TEST_CONTRACT_ADDRESS = '0x89a67490d70452ee7d7be9f775e708ee70058f42';
+const CONTRACT_ADDRESS = '0xeAfc7d1a89719c3BA25fbC3709587f9F985DAf7B';
 
 const PROJECT_ID = 'f2166ad1fd2043a3b5920fa9a47dee0c';
 const PROJECT_SECRET = 'a0ef7fece8e24f20b851d44463d68428';
 
 const ACCOUNT_ADDRESS = '0x76a15F9B153765cBD6fc737969b65116D1860Bc1';
-const PRIVATE_KEY =
-  '0x3e9ae3e87cb7e926f9eaf0d87429d9a17283fd504617975d65f2c88b7bda83f6';
+const PRIVATE_KEY = '0x3e9ae3e87cb7e926f9eaf0d87429d9a17283fd504617975d65f2c88b7bda83f6';
 
 const provider = new InfuraProvider('ropsten', {
   projectId: PROJECT_ID,
@@ -46,7 +45,6 @@ const main = async () => {
     });
     // Need to convert in String
     const stringifyData = JSON.stringify(data);
-    console.log(stringifyData);
 
     // Create new random account
     const account = {
@@ -54,34 +52,23 @@ const main = async () => {
       privateKey: PRIVATE_KEY,
     };
     const accountBalance = await getBalanceAsync(account.address);
-    console.log(accountBalance);
-
     const wallet = new ethers.Wallet(account.privateKey, provider);
-    const contract = new ethers.Contract(TEST_CONTRACT_ADDRESS, TestContract, wallet);
+    const contract = new ethers.Contract(CONTRACT_ADDRESS, Contract, wallet);
 
     // Generate IPFS
-    const client = await IPFS.create({
-      host: 'ipfs.io',
-      protocol: 'https',
-      apiPath: 'ipfs'
-    });
-    //console.log(client)
-
-    /*const result = await client.files.read('https://ipfs.io/ipfs/QmZqXJgxkP4gAkWid2xNg68bPrrsa7zHqzU3o4F7R8E3Hz')
-    console.log(result)*/
-
-    const ipfsAddr = '/ipfs/QmZqXJgxkP4gAkWid2xNg68bPrrsa7zHqzU3o4F7R8E3Hz'
+    const client = await IPFS.create();
+    const { cid } = await client.add(stringifyData);
+    console.log(stringifyData)
     
-    /*await client.name.publish(ipfsAddr).then(function (res) {
-      console.log(`https://ipfs.io/ipns/${res.name}`)
-    })*/
+    client.name.publish(`/ipfs/${ cid.toString() }`).then(function (res) {
+      console.log(`https://gateway.ipfs.io/ipns/${res.name}`)
+    });
 
-
-    /*(async function () {
-      const tx = await contract.set("{symbol:AAPL,high:173.63,low:170.13,open:172.36,close:171.83,date:2022-04-06T00:00:00.000Z}");
+    (async function () {
+      const tx = await contract.set(stringifyData);
       const result = await tx.wait();
       console.log(`https://ropsten.etherscan.io/tx/${result.transactionHash}`);
-    })();*/
+    })();
   }
 };
 
